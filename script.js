98331310d6651c664cc5b3aa04fbf63f0218dec3
kalimuthuk1092-png <kalimuthu.k1092@gmail.com>
2026-02-11 15:26:45 +0530

    protfolio

---
+++ script.js
@@ -0,0 +1,23 @@
+const texts = [
+    "MEP Engineer",
+    "HVAC Site Supervisor",
+    "AutoCAD & Revit Specialist"
+];
+
+(function type() {
+    if (count === texts.length) {
+        count = 0;
+    }
+    currentText = texts[count];
+    letter = currentText.slice(0, ++index);
+
+    document.getElementById("typing").textContent = letter;
+
+    if (letter.length === currentText.length) {
+        count++;
+        index = 0;
+        setTimeout(type, 1000);
+    } else {
+        setTimeout(type, 100);
+    }
+})();
